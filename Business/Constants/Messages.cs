d1714ec9945e370da4539ca8b0d5b13f7160662d
etocans <ertugkececi@gmail.com>
2022-07-31 01:10:10 +0300

    Result yapısı eklendi
    Dönüşlere Result entegre edildi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string MaintanceTime = "Sistem bakımda";
+
+        public static string CarInvalidName = "Araba ismi minimum 2 karakter olmalıdır";
+        public static string CarInvalidDailyPrice = "Araba günlük fiyatı 0'dan büyük olmalıdır";
+        public static string CarAdded = "Araba eklendi";
+        public static string CarDeleted = "Araba silindi";
+        public static string CarUpdated = "Araba güncellendi";
+        public static string CarListed = "Arabalar listelendi";
+
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandListed = "Markalar listelendi";
+
+        public static string ColorAdded = "Renk eklendi";
+        public static string ColorDeleted = "Renk silindi";
+        public static string ColorUpdated = "Renk güncellendi";
+        public static string ColorListed = "Renkler listelendi";
+    }
+}
+
